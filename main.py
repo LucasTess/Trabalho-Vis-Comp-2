20a70697470ff972a3b700a8e3c8318bfae18be8
LucasTess <109961869+LucasTess@users.noreply.github.com>
2025-02-06 14:47:15 -0300

    Coloquei os códigos que ja tinha referente a cálculo de normalização, DLT e matriz H. Cabe agora discutir sobre qual vei ser a hierarquia do código, visto que variáveis criadas em funções separadas são usadas na função que realmente cria a matriz de homografia, a matriz H.

--- main.py
+++ main.py
@@ -15,31 +15,55 @@ import cv2 as cv
 # Saída: norm_points (pontos normalizados)
 #        T (matriz de normalização)
 def normalize_points(points):
-    
-    
-    
-    return norm_points, T
+    # Calculate centroid
+    centroid = np.mean(points, axis=0)
+    # Calculate the average distance of the points having the centroid as origin
+    distances = np.linalg.norm(points - centroid, axis=1)
+    avg_distance = np.mean(distances)
+    # Define the scale to have the average distance as sqrt(2)
+    scale = np.sqrt(2) / avg_distance
+    # Define the normalization matrix (similar transformation)
+    T = np.array([[scale, 0, -scale * centroid[0]],
+                  [0, scale, -scale * centroid[1]],
+                  [0, 0, 1]])
+    # Normalize points
+    norm_pts = (T @ np.hstack((points, np.ones((points.shape[0], 1)))).T).T[:, :2]
+    return T, norm_pts
 
 # Função para montar a matriz A do sistema de equações do DLT
 # Entrada: pts1, pts2 (pontos "pts1" da primeira imagem e pontos "pts2" da segunda imagem que atendem a pts2=H.pts1)
 # Saída: A (matriz com as duas ou três linhas resultantes da relação pts2 x H.pts1 = 0)
 def compute_A(pts1, pts2):
-    
+    # Add homogeneous coordinates
+    pts1 =np.hstack((pts1,np.ones((pts1.shape[0],1)))).T
+    pts2 =np.hstack((pts2,np.ones((pts2.shape[0],1)))).T
+    # Compute matrix A
+    npoints = pts1.shape[1]
+    A = np.zeros((3*npoints,9))
+    for k in range(npoints):
+      A[3*k,3:6] = -pts2[2,k]*pts1[:,k]
+      A[3*k,6:9] = pts2[1,k]*pts1[:,k]
+      A[3*k+1,0:3] = pts2[2,k]*pts1[:,k]
+      A[3*k+1,6:9] = -pts2[0,k]*pts1[:,k]
+      A[3*k+2,0:3] = -pts2[1,k]*pts1[:,k]
+      A[3*k+2,3:6] = pts2[0,k]*pts1[:,k]   
     return A
 
 # Função do DLT Normalizado
 # Entrada: pts1, pts2 (pontos "pts1" da primeira imagem e pontos "pts2" da segunda imagem que atendem a pts2=H.pts1)
 # Saída: H (matriz de homografia estimada)
-def compute_normalized_dlt(pts1, pts2):
+def compute_normalized_dlt(pts1, pts2, A,T1,T2):
 
     # Normaliza pontos
 
     # Constrói o sistema de equações empilhando a matrix A de cada par de pontos correspondentes normalizados
 
     # Calcula o SVD da matriz A_empilhada e estima a homografia H_normalizada 
-
+    U,S,Vt = np.linalg.svd(A)
+    h = Vt[-1,:]
+    H_matrix = np.reshape(h,(3,3))
     # Denormaliza H_normalizada e obtém H
-
+    H = np.linalg.inv(T2) @ H_matrix @ T1
 
     return H
 
